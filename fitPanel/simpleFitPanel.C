06bc4ef4ccf2f21ae9fd6e482b60083b4dda32a9
Iliana Betsou <iliana.betsou@cern.ch>
2018-05-23 15:57:55 +0200

    Changes in comboboxes and fitting function

--- fitPanel/simpleFitPanel.C
+++ fitPanel/simpleFitPanel.C
@@ -21,19 +21,23 @@ struct FitPanelModel {
    std::vector<ComboBoxItem> fDataSet;
    std::string fSelectDataId;
    std::vector<ComboBoxItem> fTypeFunc;
-   std::string fSelectTypeId;
-   std::vector<ComboBoxItem> fTypeXY;
    std::string fSelectXYId;
    std::vector<ComboBoxItem> fMethod;
    std::string fSelectMethodId;
    std::string realfunc;
-   std::string _slider;
 
 
    // all combo items for all methods
+
+   //Minimization Tab
    std::vector<std::vector<ComboBoxItem>> fMethodMinAll;
+   //Fit Function --- Type
+   std::vector<std::vector<ComboBoxItem>> fTypeXYAll;;
 
    std::vector<ComboBoxItem> fMethodMin;
+   std::vector<ComboBoxItem> fTypeXY;
+
+   std::string fSelectTypeId;
    std::string fSelectMethodMinId;
 
    float fMinRange{0};
@@ -47,6 +51,8 @@ struct FitPanelModel {
    bool fWeights{false};
    bool fBins{false};
    int fLibrary{0};
+   //convert fSelectTypeID from string to int
+   int fTypeId = atoi(fSelectTypeId.c_str());
 
 };
 
@@ -78,6 +84,7 @@ public:
          fWindow->Send(fConnId, "INITDONE");
 
          FitPanelModel model;
+         //ComboBox for Data Set
          model.fDataSet.push_back(ComboBoxItem("1", "No Selection"));
          model.fDataSet.push_back(ComboBoxItem("2", "TH1F::hpx"));
          model.fDataSet.push_back(ComboBoxItem("3", "TH2F::hpxhpy"));
@@ -85,21 +92,45 @@ public:
          model.fDataSet.push_back(ComboBoxItem("5", "TNtuple::ntuple"));
          model.fSelectDataId = "2";
 
+         //ComboBox for Fit Function --- Type
+         model.fTypeFunc.push_back(ComboBoxItem("0", "Predef-1D"));
          model.fTypeFunc.push_back(ComboBoxItem("1", "User Func"));
-         model.fTypeFunc.push_back(ComboBoxItem("2", "Predef-1D"));
-         model.fSelectTypeId = "1";
-
-         model.fTypeXY.push_back(ComboBoxItem("1", "gaus"));
-         model.fTypeXY.push_back(ComboBoxItem("2", "expo"));
-         model.fTypeXY.push_back(ComboBoxItem("3", "landau"));
-         model.fTypeXY.push_back(ComboBoxItem("4", "pol1"));
+         model.fSelectTypeId = "0";
+         
+         //Sub ComboBox for Type Function
          model.fSelectXYId = "1";
 
+         //corresponds when Type == Predef-1D (fSelectedTypeID == 0)
+         model.fTypeXYAll.emplace_back();
+         std::vector<ComboBoxItem> &vec0 = model.fTypeXYAll.back();
+         vec0.push_back(ComboBoxItem("1", "gaus"));
+         vec0.push_back(ComboBoxItem("2", "expo"));
+         vec0.push_back(ComboBoxItem("3", "landau"));
+         vec0.push_back(ComboBoxItem("4", "pol1"));
+
+
+         //corresponds when Type == User Func (fSelectedTypeID == 1)
+         model.fTypeXYAll.emplace_back();
+         std::vector<ComboBoxItem> &vec1 = model.fTypeXYAll.back();
+         vec1.push_back(ComboBoxItem("1", "chebyshev0"));
+         vec1.push_back(ComboBoxItem("2", "chebyshev1"));
+         vec1.push_back(ComboBoxItem("3", "chebyshev2"));
+         vec1.push_back(ComboBoxItem("4", "chebyshev3"));
+         vec1.push_back(ComboBoxItem("5", "chebyshev4"));
+         vec1.push_back(ComboBoxItem("6", "chebyshev5"));
+         vec1.push_back(ComboBoxItem("7", "chebyshev6"));
+         vec1.push_back(ComboBoxItem("8", "chebyshev7"));
+         vec1.push_back(ComboBoxItem("9", "chebyshev8"));
+         vec1.push_back(ComboBoxItem("10", "chebyshev9"));
+
+         //ComboBox for General Tab --- Method
          model.fMethod.push_back(ComboBoxItem("1", "Chi-square"));
          model.fMethod.push_back(ComboBoxItem("2", "Binned Likelihood"));
          model.fSelectMethodId = "1";
 
+         //Sub ComboBox for Minimization Tab --- Method
          model.fSelectMethodMinId = "1";
+
          // corresponds to library == 0
          model.fMethodMinAll.emplace_back();
          std::vector<ComboBoxItem> &vect0 = model.fMethodMinAll.back();
@@ -111,8 +142,10 @@ public:
          // corresponds to library == 1
          model.fMethodMinAll.emplace_back();
          std::vector<ComboBoxItem> &vect1 = model.fMethodMinAll.back();
-         vect1.push_back(ComboBoxItem("1", "Lib1_1"));
-         vect1.push_back(ComboBoxItem("2", "Lib1_2"));
+         vect1.push_back(ComboBoxItem("1", "MIGRAD"));
+         vect1.push_back(ComboBoxItem("2", "SIMPLEX"));
+         vect1.push_back(ComboBoxItem("3", "SCAN"));
+         vect1.push_back(ComboBoxItem("4", "Combination"));
 
          // corresponds to library == 2
          model.fMethodMinAll.emplace_back();
@@ -132,7 +165,7 @@ public:
 
          // select items list for initial display
          model.fMethodMin = model.fMethodMinAll[model.fLibrary];
-
+         model.fTypeXY = model.fTypeXYAll[model.fTypeId];
 
          model.fMinRange = -4;
          model.fMaxRange = 4;
@@ -141,6 +174,7 @@ public:
             model.fMaxRange = fHist->GetXaxis()->GetXmax();
          }
 
+         //defined values
          model.fStep = (model.fMaxRange - model.fMinRange) / 100;
          model.fRange[0] = model.fMinRange;
          model.fRange[1] = model.fMaxRange;
@@ -158,10 +192,12 @@ public:
          fWindow->Send(fConnId, std::string("MODEL:") + json.Data());
 
          //printf("fLibrary is (%d)\n", model.fLibrary);
-         printf("Slider %s\n", model._slider.c_str() );
+         //printf("fTypeId is %d\n", model.fTypeId );
+
 
          return;
       }
+      //FITTING
       if (arg.find("DOFIT:") == 0) {
          std::string arg1 = arg;
          arg1.erase(0,6);
@@ -172,29 +208,14 @@ public:
 
             if (!obj->realfunc.empty()) {
                printf("GOT realfunc: %s\n", obj->realfunc.c_str());
-            } else if(obj->fSelectXYId == "1"){
-               obj->realfunc = "gaus";
-               printf("%s\n", obj->realfunc.c_str());
-            }
-            else if (obj->fSelectXYId == "2"){
-               obj->realfunc = "expo";
-               printf("%s\n", obj->realfunc.c_str());
-            }
-            else if (obj->fSelectXYId == "3"){
-               obj->realfunc = "landau";
-               printf("%s\n", obj->realfunc.c_str());
-            }
-            else if (obj->fSelectXYId == "4"){
-               obj->realfunc = "pol1";
-               printf("%s\n", obj->realfunc.c_str());
-            }
+            } 
             else {
                obj->realfunc = "gaus";
                printf("%s\n", obj->realfunc.c_str());
             }
 
             if (fHist)
-               fHist->Fit(obj->realfunc.c_str(), "", "", obj->fRange[0], obj->fRange[1]);
+               fHist->Fit(obj->realfunc.c_str(), "I", "", obj->fRange[0], obj->fRange[1]);
             delete obj;
          }
 
@@ -233,7 +254,7 @@ void simpleFitPanel()
    auto panel = new FitPanel();
 
    TH1F *hpx = new TH1F("hpx","This is the px distribution",100,-4,4);
-   hpx->FillRandom("gaus", 10000);
+   hpx->FillRandom("expo", 10000);
    hpx->Draw();
 
    panel->AssignHistogram(hpx);
