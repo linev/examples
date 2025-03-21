30e4381634b5654632c7c2d69a957acdd23397b9
Sergey Linev <S.Linev@gsi.de>
2025-03-21 13:01:41 +0100

    Convert openui5 example

--- openui5/fclWithRouting.C
+++ openui5/fclWithRouting.C
@@ -1,63 +1,47 @@
-/// \file
-/// \ingroup tutorial_http
-///  This program creates and fills one and two dimensional histogram
-///  Macro used to demonstrate usage of custom HTML page in custom.htm
-///  One can use plain JavaScript to assign different actions with HTML buttons
+///  This demo starts openui5 widget inside RWebWindow
 ///
-/// \macro_code
 ///
 /// \author Sergey Linev
 
 
-#include <ROOT/RWebWindowsManager.hxx>
+#include <ROOT/RWebWindow.hxx>
 
 class WHandler {
 private:
-   std::shared_ptr<ROOT::Experimental::RWebWindow>  fWindow;
-   unsigned fConnId{0};
+   std::shared_ptr<ROOT::RWebWindow>  fWindow;
+   unsigned fConnId = 0;
 
 public:
-   WHandler() {};
+   WHandler() {}
 
-   virtual ~WHandler() { printf("Destructor!!!!\n"); }
+   void ProcessConnection(unsigned connid)
+   {
+      fConnId = connid;
+      printf("connection established %u\n", fConnId);
+      fWindow->Send(fConnId, "INITDONE");
+   }
 
    void ProcessData(unsigned connid, const std::string &arg)
    {
-      if (arg == "CONN_READY") {
-         fConnId = connid;
-         printf("connection established %u\n", fConnId);
-         fWindow->Send(fConnId, "INITDONE");
-
-         return;
-      }
-
-      if (arg == "CONN_CLOSED") {
-         printf("connection closed\n");
-         fConnId = 0;
-         return;
-      }
-
       printf("Get msg %s \n", arg.c_str());
-
    }
 
-   void popupTextTest(const std::string &where = "")
+   void popupWindow()
    {
-
-      fWindow =  ROOT::Experimental::RWebWindowsManager::Instance()->CreateWindow();
-
-      // this is very important, it defines name of openui5 widget, which
-      // will run on the client side
-      // fWindow->SetPanelName("TextTest");
+      fWindow = ROOT::RWebWindow::Create();
 
       fWindow->SetDefaultPage("file:fclWithRouting.html");
+
+      fWindow->SetUseCurrentDir(true);
+
+      fWindow->SetConnectCallBack([this](unsigned connid) { ProcessConnection(connid); });
 
       // this is call-back, invoked when message received via websocket
       fWindow->SetDataCallBack([this](unsigned connid, const std::string &arg) { ProcessData(connid, arg); });
 
-      fWindow->SetGeometry(300, 500); // configure predefined geometry
+      fWindow->SetGeometry(800, 600); // configure predefined geometry
 
-      fWindow->Show(where);
+      fWindow->Show();
    }
 
 };
@@ -68,5 +52,5 @@ void fclWithRouting()
 {
    handler = new WHandler();
 
-   handler->popupTextTest();
+   handler->popupWindow();
 }
